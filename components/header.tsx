824db388e0baedb96cc98ab7b8b992b327c6451b
litlatte <hamzahaddouedev@gmail.cmo>
2022-06-29 00:05:37 +0200

    Added hide email

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import { signIn, signOut, useSession } from "next-auth/react"
 import styles from "./header.module.css"
+import Image from "next/image"
+import { useState } from "react"
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
@@ -8,6 +10,7 @@ import styles from "./header.module.css"
 export default function Header() {
   const { data: session, status } = useSession()
   const loading = status === "loading"
+  let [showEmail, setShowEmail] = useState<'show'|'hide'>('hide');
 
   return (
     <header>
@@ -48,7 +51,10 @@ export default function Header() {
               <span className={styles.signedInText}>
                 <small>Signed in as</small>
                 <br />
-                <strong>{session.user.email ?? session.user.name}</strong>
+                <strong className={`${showEmail=='show'? '':'bg-gray-800 text-gray-800 rounded-full shadow opacity-40 select-none'} mr-2 transition duration-100`}>
+                  {session.user.email ?? session.user.name}
+                </strong>
+                <Image onClick={()=>setShowEmail(showEmail=='show'?'hide':'show')} className="!mt-1 hover:cursor-pointer" src={`/img/${showEmail}.png`} height={20} width={20} />
               </span>
               <a
                 href={`/api/auth/signout`}
