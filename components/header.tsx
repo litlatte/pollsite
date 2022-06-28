789576629e3fff25c345322d6633a0ca7f5d46f9
litlatte <hamzahaddouedev@gmail.cmo>
2022-06-29 00:26:18 +0200

    Small graphic fix

--- components/header.tsx
+++ components/header.tsx
@@ -51,7 +51,7 @@ export default function Header() {
               <span className={styles.signedInText}>
                 <small>Signed in as</small>
                 <br />
-                <strong className={`${showEmail=='show'? '':'bg-gray-800 text-gray-800 rounded-full shadow opacity-40 select-none'} mr-2 transition duration-100`}>
+                <strong className={`${showEmail=='show'? '':'bg-gray-800 text-gray-800 rounded-lg shadow opacity-40 select-none'} mr-2 transition duration-100`}>
                   {session.user.email ?? session.user.name}
                 </strong>
                 <Image onClick={()=>setShowEmail(showEmail=='show'?'hide':'show')} className="!mt-1 hover:cursor-pointer" src={`/img/${showEmail}.png`} height={20} width={20} />
